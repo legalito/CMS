46747197c5f4d7982332fb364a67ec55712bb01f
Pierre <fardeau.pierrot@gmail.com>
2020-09-23 14:40:07 +0200

    jd

---
+++ php/partie4/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link type="text/css" rel="stylesheet" href="stylesheet.css">
+  <title>Title</title>
+</head>
+
+<body>
+<div class="grid">
+  <div class="menu">
+    <li><a href="#" title="voir le profil">Profil</a></li>
+    <li><a href="#" title="éditer le profil">Editer le profil</a></li>
+    <li><a href="#" title="publier un article">Ajouter un article</a></li>
+    <li><a href="#" title="voir les articles publiés">Articles publiés</a></li>
+    <li><a href="#" title="voir les recommendations">Recommendations</a></li>
+  </div>
+  <div class="article_titre">
+    <a> information sur l'article</a>
+  </div>
+  <div class="article"></div>
+</div>
+</body>
+
+
+
+
+</html>
