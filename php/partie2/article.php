22625cb0e5a63bec60cbfe9b547e2a80734905ae
hugo <hlegal44@hotmail.fr>
2020-09-23 14:19:34 +0200

    oui

--- php/partie2/article.php
+++ php/partie2/article.php
@@ -2,12 +2,12 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="Style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
   <title>Article</title>
 </head>
 <body class="body">
   <div class="nav">
-    <img src="../../photos/Grandmere_cafe_logo.png" class="logo" alt="Logo">
+    <img src="../../photos/grandmere_cafe_logo.png" class="logo" alt="Logo">
 
   </div>
   <div class="Art_En_Cour"> en cour </div>
@@ -17,9 +17,16 @@
   </div>
   <div class="Art">
     <div class="TitreArticle">
+      <h2>Titre :<?php
+        include_once ("../db.php");
+        $result = get_informations_from_db('SELECT * FROM articles');
+        echo $result['titre'];
+        ?> </h2>
+
     </div>
     <img class="photo">
-    <div class="resume"></div>
+    <div class="resume"><?php echo $result['resume']; ?></div>
+
 
 
   </div>
